1183057a64ae2a45249108f0d872d2750ff95eef
limengxuan <391013634@qq.com>
2025-02-11 17:52:01 +0800

    add supporters
    Signed-off-by: limengxuan <391013634@qq.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 const { themes } = require("prism-react-renderer");
-//const lightTheme = themes.github;
-//const darkTheme = themes.dracula;
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 const defaultLocale = 'en';
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
@@ -91,7 +91,6 @@ module.exports = {
       },
     },
   ],
-  /*
   plugins: [
     [
       './src/plugins/changelog/index.js',
@@ -109,7 +108,7 @@ module.exports = {
         onInlineAuthors: 'warn',
       },
     ],
-  ],*/
+  ],
   themeConfig: {
     announcementBar: {
       id: "start",
