9b706ea49ebe6b9de2d1a785d4835e903605420e
limengxuan <391013634@qq.com>
2025-04-24 17:42:37 +0800

    update
    Signed-off-by: limengxuan <391013634@qq.com>

--- src/data/supporters.js
+++ src/data/supporters.js
@@ -8,6 +8,13 @@ import iFly from "../../static/img/supporters/iFly.png";
 import huawei from "../../static/img/supporters/huawei.png";
 import qxzg from "../../static/img/supporters/qxzg-ai.jpg";
 import riseunion from "../../static/img/supporters/riseunion.png"
+import ascend from "../../static/img/supporters/ascend.png"
+import cambricon from "../../static/img/supporters/cambricon.png"
+import enflame from "../../static/img/supporters/enflame.png"
+import hygon from "../../static/img/supporters/hygon.png"
+import iluvatar from "../../static/img/supporters/iluvatar.png"
+import metax from "../../static/img/supporters/metax.png"
+import mthreads from "../../static/img/supporters/mthreads.png"
 
 const supportersData = [
   {
@@ -50,6 +57,34 @@ const supportersData = [
     logo: riseunion,
     alt: "riseunion",
   },
+  {
+    logo: ascend,
+    alt: "ascend",
+  },
+  {
+    logo: cambricon,
+    alt: "cambricon",
+  },
+  {
+    logo: enflame,
+    alt: "enflame",
+  },
+  {
+    logo: hygon,
+    alt: "hygon",
+  },
+  {
+    logo: iluvatar,
+    alt: "iluvatar",
+  },
+  {
+    logo: metax,
+    alt: "metax",
+  },
+  {
+    logo: mthreads,
+    alt: "mthreads"
+  }
 ];
 
 export default supportersData;
